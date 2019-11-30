8c338b1eb742689d2c37b21a623b5e1b431548e7
vonbarown <vonielbrown@pursuit.org>
2019-11-30 12:16:10 -0500

    need to fix operations logic

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -14,39 +14,60 @@ class App extends React.Component {
     this.state = this.initialState;
   }
 
+  // previousValue = event => {};
+
   handleInput = event => {
     this.setState({
       previousValue: this.state.displayValue,
-      displayValue: event.target.innerText
+      displayValue: parseFloat(event.target.innerText)
     });
   };
 
+  allClear = () => {
+    this.setState(this.initialState);
+  };
+
   handleOperation = event => {
+    this.ops();
+
     this.setState({
-      displayValue: this.state.displayValue,
       operation: event.target.innerText
+      // displayValue: this.ops()
     });
+  };
+
+  ops = () => {
+    const { operation, displayValue, previousValue } = this.state;
+
+    if (operation === "+") {
+      this.setState({
+        displayValue: previousValue + displayValue
+      });
+    }
   };
 
   handleSubmit = event => event.preventDefault();
 
   render() {
-    // const { displayValue } = this.state;
+    const { operation } = this.state;
 
     console.log(this.state);
 
     return (
       <div className="App">
         <h1>Simple Calculator</h1>
-        <button>Helpbvebj</button>
         <div className="display"></div>
         <form onSubmit={this.handleSubmit}>
           <div className="calc">
             <div className="buttons">
-              <button className="button col-3" onClick={this.handleOperation}>
+              <button className="button col-3" onClick={this.allClear}>
                 AC
               </button>
-              <button className="button col-3" onClick={this.handleOperation}>
+              <button
+                className="button col-3"
+                onClick={this.handleOperation}
+                value={operation}
+              >
                 %
               </button>
               <button className="button col-3" onClick={this.handleOperation}>
@@ -100,6 +121,7 @@ class App extends React.Component {
               <button
                 className="button col-3 orange"
                 onClick={this.handleOperation}
+                value={operation}
               >
                 +
               </button>
