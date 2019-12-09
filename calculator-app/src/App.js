c27af0c32e22948e049a00885cb2e6d68445a7b4
Bpmauge <briahanamauge@pursuit.org>
2019-12-09 14:11:08 -0500

    tested the add and equal buttons

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -2,10 +2,32 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-  constructor(){
-  super();
-}
+  constructor() {
+    super();
+    this.state = {
+      num1:'',
+      num2:'',
+      result: 0,
+      method: ''
+    }
+  }
+  addClick = () => {
+ 
+    this.setState({
+      operation: 'addition',
+
+    })
+  }
+  equalsClick = () => {
+    let {num1, num2, operation} = this.state
+    if (operation === 'addition') {
+      this.setState({
+        result: parseInt(num1) + parseInt(num2)
+      })
+    }
+  }
   render() {
+    let { result } = this.state
     return (
       <div className="App">
         <div className='calculator-whole'>
@@ -31,12 +53,13 @@ class App extends React.Component {
           <button className='button' id='num1'>1</button>
           <button className='button' id='num2'>2</button>
           <button className='button' id='num3'>3</button>
-          <button className='button orange' id='addition'>+</button>
+          <button className='button orange' id='addition' onClick = {this.addClick}>+</button>
           <button className='button' id='num0'>0</button>
           <button className='button' id='decimal'>.</button>
-          <button className='button orange' id='equals'>=</button>
+          <button className='button orange' id='equals' onClick = {this.equalsClick}>=</button>
           <button className='button' id='clear'>C</button>
         </div>
+        <p>{result}</p>
       </div>
     );
   }
