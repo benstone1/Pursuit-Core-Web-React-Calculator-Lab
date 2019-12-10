4618b20289f182604e29f541a8cc9059943f1718
Bpmauge <briahanamauge@pursuit.org>
2019-12-09 20:26:02 -0500

    added sub and division function

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -5,61 +5,106 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      num1:'',
-      num2:'',
+      num1: '',
+      num2: '',
       result: 0,
-      method: ''
+      method: null
     }
   }
-  addClick = () => {
- 
+  allClearClick = () => {
     this.setState({
-      operation: 'addition',
+      num1: '',
+      num2: '',
+      result: 0,
+      method: null
+    })
+  }
+  addClick = () => {
+    this.setState({
+      method: 'addition'
+    })
+  }
+  subClick = () => {
+    this.setState({
+      method: 'subtraction',
 
     })
   }
+  divClick = () => {
+      this.setState({
+        method: 'division',
+  
+      })
+  }
   equalsClick = () => {
-    let {num1, num2, operation} = this.state
-    if (operation === 'addition') {
+    let { num1, num2, method } = this.state
+    if (method === 'addition') {
       this.setState({
         result: parseInt(num1) + parseInt(num2)
       })
+    } else if(method === 'subtraction'){
+      this.setState({
+        result: parseInt(num1) - parseInt(num2)
+      })}
+      else if(method === 'division') {
+        if (num2 === 0){
+          this.setState({
+            result: 'ERROR'
+          })
+        }
+        else{
+        this.setState({
+          result: parseInt(num1) / parseInt(num2)
+        })
+      }
     }
+    }
+  
+  numberClick = (element) => {
+    let {num1, num2, method} = this.state;
+    if(method === null){
+    this.setState({
+      num1: num1 + element.target.value
+    })
+  } else{
+    this.setState({
+      num2: num2 + element.target.value
+    })
+  }
   }
   render() {
-    let { result } = this.state
+    let { num1, result } = this.state
     return (
       <div className="App">
         <div className='calculator-whole'>
 
           <div className='stage'>
-            <h1>01234</h1>
+            <h1>{result}</h1>
             {/* <input className = 'input-stage' type = 'text'/> */}
 
           </div>
-          <button className='button' id='all-clear'>AC</button>
+          <input type='button' className='button' id='all-clear' onClick = {this.allClearClick} value='AC' />
 
-          <button className='button' id='percent'>%</button>
-          <button className='button' id='negative'>±</button>
-          <button className='button orange' id='divide'>÷</button>
-          <button className='button' id='num7'>7</button>
-          <button className='button' id='num8'>8</button>
-          <button className='button' id='num9'>9</button>
-          <button className='button orange' id='multiply'>x</button>
-          <button className='button' id='num4'>4</button>
-          <button className='button' id='num5'>5</button>
-          <button className='button' id='num6'>6</button>
-          <button className='button orange' id='subtract'>-</button>
-          <button className='button' id='num1'>1</button>
-          <button className='button' id='num2'>2</button>
-          <button className='button' id='num3'>3</button>
-          <button className='button orange' id='addition' onClick = {this.addClick}>+</button>
-          <button className='button' id='num0'>0</button>
-          <button className='button' id='decimal'>.</button>
-          <button className='button orange' id='equals' onClick = {this.equalsClick}>=</button>
-          <button className='button' id='clear'>C</button>
+          <input type='button' className='button' id='percent' value='%' />
+          <input type='button' className='button' id='negative' value='±' />
+          <input type='button' className='button orange' id='divide' onClick={this.divClick} value='÷' />
+          <input type='button' className='button' id='num7' onClick = {this.numberClick} value='7' />
+          <input type='button' className='button' id='num8' onClick = {this.numberClick} value='8' />
+          <input type='button' className='button' id='num9' onClick = {this.numberClick} value='9' />
+          <input type='button' className='button orange' id='multiply' value='x' />
+          <input type='button' className='button' id='num4' onClick = {this.numberClick} value='4' />
+          <input type='button' className='button' id='num5' onClick = {this.numberClick} value='5' />
+          <input type='button' className='button' id='num6' onClick = {this.numberClick} value='6' />
+          <input type='button' className='button orange' id='subtract' onClick={this.subClick} value='-' />
+          <input type='button' className='button' id='num1' value='1' onClick = {this.numberClick}/>
+          <input type='button' className='button' id='num2' value='2' onClick = {this.numberClick}/>
+          <input type='button' className='button' id='num3' value='3' onClick = {this.numberClick}/>
+          <input type='button' className='button orange' id='addition' onClick={this.addClick} value='+' />
+          <input type='button' className='button' id='num0' onClick = {this.numberClick} value='0' />
+          <input type='button' className='button' id='decimal' onClick = {this.numberClick} value='.' />
+          <input type='button' className='button orange' id='equals' onClick={this.equalsClick} value='=' />
+          <input type='button' className='button' id='clear' value='C' />
         </div>
-        <p>{result}</p>
       </div>
     );
   }
