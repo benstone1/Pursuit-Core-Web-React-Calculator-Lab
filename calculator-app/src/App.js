5c515178ec7ac46a7e35c4db25f7e571200da935
Bpmauge <briahanamauge@pursuit.org>
2019-12-13 00:40:53 -0500

    percentage function works and passed props to calculator component

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Calculator from './Components/buttons'
 
 class App extends React.Component {
   constructor() {
@@ -27,13 +28,27 @@ class App extends React.Component {
       method: null
     })
   }
+  percentageClick = () => {
+    let { num1, num2, method } = this.state;
+    if (method === null) {
+      this.setState({
+        num1: num1 / 100,
+        display: num1 / 100
+
+      })
+    } else {
+      this.setState({
+        num2: num2 / 100,
+        display: num2 / 100
+      })
+    }
+  }
   negateClick = () => {
-  
     let { num1, num2, method } = this.state;
     if (method === null) {
       this.setState({
         num1: 0 - num1,
-        display: 0 - num1 
+        display: 0 - num1
       })
     } else {
       this.setState({
@@ -43,7 +58,6 @@ class App extends React.Component {
     }
   }
   addClick = () => {
-    
     this.setState({
       method: 'addition'
     })
@@ -70,25 +84,30 @@ class App extends React.Component {
     let { num1, num2, method } = this.state
     if (method === 'addition') {
       this.setState({
-        display: parseInt(num1) + parseInt(num2)
+        display: Number(num1) + Number(num2),
+        method: null
       })
     } else if (method === 'subtraction') {
       this.setState({
-        display: parseInt(num1) - parseInt(num2)
+        display: Number(num1) - Number(num2),
+        method: null
       })
     } else if (method === 'multiplication') {
       this.setState({
-        display: parseInt(num1) * parseInt(num2)
+        display: Number(num1) * Number(num2),
+        method: null
       })
     } else {
-      if (parseInt(num2) === 0) {
+      if (Number(num2) === 0) {
         this.setState({
-          display: 'ERROR'
+          display: 'ERROR',
+          method: null
         })
       }
       else {
         this.setState({
-          display: parseInt(num1) / parseInt(num2)
+          display: Number(num1) / Number(num2),
+          method: null
         })
       }
     }
@@ -110,38 +129,23 @@ class App extends React.Component {
   }
 
   render() {
-    let { display } = this.state
+    let { display, method } = this.state
     return (
       <div className="App">
-        <div className='calculator-whole'>
-
-          <div className='stage'>
-            {/* <h1>{result}</h1> */}
-            <input className='input-stage' type='text' value={display} readOnly />
-
-          </div>
-          <input type='button' className='button' id='all-clear' onClick={this.allClearClick} value='AC' />
-
-          <input type='button' className='button' id='percent' value='%' />
-          <input type='button' className='button' id='negative' onClick = {this.negateClick} value='±' />
-          <input type='button' className='button orange' id='divide' onClick={this.divClick} value='÷' />
-          <input type='button' className='button' id='num7' onClick={this.numberClick} value='7' />
-          <input type='button' className='button' id='num8' onClick={this.numberClick} value='8' />
-          <input type='button' className='button' id='num9' onClick={this.numberClick} value='9' />
-          <input type='button' className='button orange' id='multiply' onClick={this.mulClick} value='x' />
-          <input type='button' className='button' id='num4' onClick={this.numberClick} value='4' />
-          <input type='button' className='button' id='num5' onClick={this.numberClick} value='5' />
-          <input type='button' className='button' id='num6' onClick={this.numberClick} value='6' />
-          <input type='button' className='button orange' id='subtract' onClick={this.subClick} value='-' />
-          <input type='button' className='button' id='num1' value='1' onClick={this.numberClick} />
-          <input type='button' className='button' id='num2' value='2' onClick={this.numberClick} />
-          <input type='button' className='button' id='num3' value='3' onClick={this.numberClick} />
-          <input type='button' className='button orange' id='addition' onClick={this.addClick} value='+' />
-          <input type='button' className='button' id='num0' onClick={this.numberClick} value='0' />
-          <input type='button' className='button' id='decimal' onClick={this.numberClick} value='.' />
-          <input type='button' className='button orange' id='equals' onClick={this.equalsClick} value='=' />
-          <input type='button' className='button' id='clear' onClick={this.clearClick} value='C' />
-        </div>
+        <Calculator
+          display={display}
+          method={method}
+          numberClick={this.numberClick}
+          equalsClick={this.equalsClick}
+          mulClick={this.mulClick}
+          addClick={this.addClick}
+          subClick={this.subClick}
+          divClick={this.divClick}
+          negateClick={this.negateClick}
+          percentageClick={this.percentageClick}
+          clearClick={this.clearClick}
+          allClearClick={this.allClearClick}
+        />
       </div>
     );
   }
