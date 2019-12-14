442d19fb63c15a9174f49e4e1a918ae0d1dcaf6e
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-12-14 03:01:00 -0500

    0.1, react install, folder structure build

---
+++ src/App.js
@@ -0,0 +1,30 @@
+/*
+Joseph P. Pasaoa
+APP | React Calculator Lab
+*/
+
+
+/* IMPORTS */
+import React, { Component } from 'react';
+import './App.css';
+
+
+/* MAIN */
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+
+/* EXPORT */
+export default App;
