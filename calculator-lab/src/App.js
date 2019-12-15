a7bbc101844eeaa6ad228693e59eb24dab235e92
kadjiah <kadijahwilson@pursuit.org>
2019-12-15 10:55:13 -0500

    fixed issue with display button clicked

--- calculator-lab/src/App.js
+++ calculator-lab/src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import Result from './Components/Result'
-import Buttons from './Components/Buttons'
+// import Result from './Components/Result'
+import Buttons from './Components/Buttons.jsx'
 import './App.css'
 
 class App extends React.Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state ={
-      displayValue: '0',
-      previousValue: null,
-      operation: null,
-      waitingForNewValue: false
+      displayValue: 0
+      // previousValue: null,
+      // operation: null,
+      // waitingForNewValue: false
     }
   }
 
-  
+  componentDidMount(){
+    console.log("component mounted")
+  }
+
+  // componentDidUpdate() {
+  //   console.log("App component updated!!");
+  // }
+
+handleInput = (e) => {
+//  console.log(e.target.value)
+
+this.setState({
+  displayValue: e.target.value
+})
+
+}
+
   render(){
+  // const { displayValue } = this.state
+  // console.log("state", {displayValue})
   return (
+
     <div className="App">
       <h1> Simple Calculator</h1>
-      <Buttons />
-    </div>
+
+      <Buttons 
+      displayValue = {this.state.displayValue}
+      handleInput = {this.handleInput}/>
+
+</div>
+
+// </div>
   );
 }
 }
--- calculator-lab/src/Components/Buttons.jsx
+++ calculator-lab/src/Components/Buttons.jsx
@@ -1,51 +1,50 @@
-import React, {Component} from 'react'
+import React from 'react'
 import './Buttons.css';
 
 
-class Buttons extends React.Component{
-    render(){
+const Buttons = (props) => {
+    console.log("props", props)
 return (
-
 <div className="clac-button">
-<input placeholder=""/>
+<input placeholder={props.displayValue}  />
 <div id="row">
-<button>AC</button>
-<button>%</button>
-<button>+/_</button>
-<button id="orange">/</button>
+<button value="AC" onClick={props.handleInput}>AC</button>
+<button value="%" onClick={props.handleInput}>%</button>
+<button value="+/-" onClick={props.handleInput}>+/-</button>
+<button value="/" onClick={props.handleInput} id="orange">/</button>
 </div>
 
 <div id="row">
-<button>7</button>
-<button>8</button>
-<button>9</button>
-<button id="orange">*</button>
+<button value="7" onClick={props.handleInput}>7</button>
+<button value="8" onClick={props.handleInput}>8</button>
+<button value="9" onClick={props.handleInput}>9</button>
+<button value="*" onClick={props.handleInput} id="orange">*</button>
 </div>
 
 <div id="row">
-<button>4</button>
-<button>5</button>
-<button>6</button>
-<button id="orange">-</button>
+<button value="4" onClick={props.handleInput}>4</button>
+<button value="5" onClick={props.handleInput}>5</button>
+<button value="6" onClick={props.handleInput}>6</button>
+<button value="-" onClick={props.handleInput} id="orange">-</button>
 </div>
 
 <div id="row">
-<button>1</button>
-<button>2</button>
-<button>3</button>
-<button id="orange">+</button>
+<button value="1" onClick={props.handleInput}>1</button>
+<button value="2" onClick={props.handleInput}>2</button>
+<button value="3" onClick={props.handleInput}>3</button>
+<button value="+" onClick={props.handleInput} id="orange">+</button>
 </div>
 
 <div id="row">
-<button>0</button>
-<button>.</button>
-<button id="orange">=</button>
+<button value="0" onClick={props.handleInput}>0</button>
+<button value="." onClick={props.handleInput}>.</button>
+<button value="=" onClick={props.handleInput} id="orange">=</button>
 </div>
 
   </div>
 )
     }
-}
+
 
 
 export default Buttons;
