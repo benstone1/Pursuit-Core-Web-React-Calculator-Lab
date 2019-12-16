1090770a3a5208f7a4b89ee33f90d8ebdf72f7f8
tbriany <brianytaveras@pursuit.org>
2019-12-15 20:23:19 -0500

    css completed

--- calculator-app/src/Components/Buttons.jsx
+++ calculator-app/src/Components/Buttons.jsx
@@ -1,45 +1,45 @@
 import React from 'react';
 
 const Buttons = (props) => {
-    const {handleInput, handleOperation, handleDisplayOperation, handleEqual} = props
+    const {displayValue, handleInput, handleOperation, handleDisplayOperation, handleEqual} = props
 
     return (
-    <div>
+    <div >
 
-      <p id="row1">0</p>
+      <p id="row1">{displayValue}</p>
 
-      <div id="row2">
-      <button onClick ={handleDisplayOperation} className="button">AC</button>
-      <button onClick ={handleOperation} className="button">%</button>
-      <button onClick ={handleDisplayOperation} className="button">+/-</button>
-      <button onClick ={handleOperation} className="button orange">/</button>
+      <div className="row">
+      <button onClick ={handleDisplayOperation} >AC</button>
+      <button onClick ={handleOperation} >%</button>
+      <button onClick ={handleDisplayOperation} >+/-</button>
+      <button onClick ={handleOperation} className="orange">/</button>
       </div>
 
-      <div id="row3">
-      <button onClick ={handleInput} className="button" >7</button>
-      <button onClick ={handleInput} className="button">8</button>
-      <button onClick ={handleInput} className="button">9</button>
-      <button onClick ={handleOperation} className="button orange">X</button>
+      <div className="row">
+      <button onClick ={handleInput} >7</button>
+      <button onClick ={handleInput}>8</button>
+      <button onClick ={handleInput} >9</button>
+      <button onClick ={handleOperation} className="orange">X</button>
       </div>
 
-      <div id="row4">
-      <button onClick ={handleInput} className="button">4</button>
-      <button onClick ={handleInput} className="button">5</button>
-      <button onClick ={handleInput} className="button">6</button>
-      <button onClick ={handleInput} className="button orange">-</button>
+      <div className="row">
+      <button onClick ={handleInput} >4</button>
+      <button onClick ={handleInput} >5</button>
+      <button onClick ={handleInput}>6</button>
+      <button onClick ={handleInput} className="orange">-</button>
       </div>
 
-      <div id="row5">
-      <button onClick ={handleInput} className="button">1</button>
-      <button onClick ={handleInput} className="button">2</button>
-      <button onClick ={handleInput} className="button">3</button>
-      <button onClick ={handleOperation} className="button orange">+</button>
+      <div className="row">
+      <button onClick ={handleInput} >1</button>
+      <button onClick ={handleInput} >2</button>
+      <button onClick ={handleInput} >3</button>
+      <button onClick ={handleOperation} className=" orange">+</button>
       </div>
 
-      <div id="row6">
-      <button onClick ={handleInput} className="button">0</button>
-      <button onClick ={handleDisplayOperation} className="button">.</button>
-      <button onClick ={handleEqual} className="button orange">=</button>
+      <div className="row">
+      <button id="zero" onClick ={handleInput} >0</button>
+      <button onClick ={handleDisplayOperation} >.</button>
+      <button onClick ={handleEqual} className=" orange">=</button>
       </div>
 
     </div>
