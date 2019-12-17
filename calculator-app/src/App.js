56df2fd71f4536abe99694715e4e707a8ebaff86
tbriany <brianytaveras@pursuit.org>
2019-12-16 21:25:36 -0500

    created function that turns number into decimal, created function that makes number negative and positive, created functions to handle operations

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -7,6 +7,7 @@ class App extends React.Component {
     super(); 
      this.state = {
         displayValue: '0',
+        displayClear: 'AC',
         previousValue: null,
         operation: null,
         waitingForNewValue: false
@@ -14,43 +15,119 @@ class App extends React.Component {
   }
 
   handleInput = (event) => {
+   const {displayValue} = this.state
+   let secondNum = event.target.value
+   let newValue = `${displayValue}${secondNum}` 
+    if (displayValue.includes('.')) {
+      this.setState({
+        displayValue: newValue
+      })
+    } else {
     this.setState({
       displayValue: event.target.value
     })
+   }
   }
 
   handleOperation = (event) => {
     const {displayValue} = this.state
     this.setState({
       operation: event.target.value,
+      displayClear: 'C',
       previousValue: displayValue,
       waitingForNewValue: true
     })
   }
 
-  handleDisplayOperation = (event) => {
-    console.log('adding clearing')
+  handleClear = (event) => {
+    let btn = event.target.value 
+    if (btn === 'AC') {
+      this.setState({
+        displayValue: '0',
+        displayClear: 'AC',
+        previousValue: null,
+        operation: null,
+        waitingForNewValue: false
+      }) } else if (btn === 'C') {
+        this.setState({
+          displayValue:'',
+          displayClear: 'AC'
+        })
+      }
+  }
+
+  handleDecimalPoint = (event) => {
+    let btn = event.target.value 
+    const {displayValue} = this.state 
+    let newValue = displayValue + '.'
+    if (btn === ".") {
+      if (displayValue.includes('.')){
+        this.setState({
+          displayValue: displayValue
+        })
+      } else {
+      this.setState({
+        displayValue: newValue
+      })
+    }
+  }
+}
+
+  handleSign = (event) => {
+    const {displayValue} = this.state 
+    let negativeNum = -Math.abs(displayValue)
+    let positiveNum = Math.abs(displayValue)
+    if (displayValue === positiveNum) {
+      this.setState({
+        displayValue: negativeNum
+      })
+    } else {
+      this.setState({
+        displayValue: positiveNum
+      })
+    }
+  }
+
+  getPercentage = () => {
+    console.log('getting percentage')
+  }
+
+  add = () => {
+    console.log('adding')
+  }
+
+  subtract = () => {
+    console.log('subtracting')
+  }
+
+  multiply = () => {
+    console.log('multiplying')
+  }
+
+  divide = () => {
+    console.log('dividing')
   }
 
   handleEqual = (event) => {
     console.log('getting result')
-    const {displayValue, previousValue, operation, waitingForNewValue} = this.state
-    let counter = 0;
+    //const {displayValue, previousValue, operation, waitingForNewValue} = this.state
+    //let counter = 0;
   }
 
   render() {
-    const {displayValue, previousValue, operation, waitingForNewValue, 
-      handleInput, handleOperation, handleDisplayOperation, handleEqual} = this.state
+    const {displayValue, displayClear} = this.state
+    const {handleInput, handleOperation, handleDecimalPoint, handleClear, 
+           handleSign, handleEqual} = this 
     return (
     <div className="App">
       <Buttons
       displayValue = {displayValue}
-      previousValue = {previousValue}
-      operation = {operation}
-      waitingForNewValue = {waitingForNewValue}
+      displayClear = {displayClear}
       handleInput = {handleInput}
       handleOperation = {handleOperation}
-      handleDisplayOperation = {handleDisplayOperation}
+      handleDecimalPoint = {handleDecimalPoint}
+      handleSign = {handleSign}
+      handleClear = {handleClear}
       handleEqual = {handleEqual}
       />
     </div>
--- calculator-app/src/Components/Buttons.jsx
+++ calculator-app/src/Components/Buttons.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const Buttons = (props) => {
-    const {displayValue, handleInput, handleOperation, handleDisplayOperation, handleEqual} = props
+    const {displayValue, displayClear, handleInput, handleOperation, 
+           handleDecimalPoint, handleSign, handleClear, handleEqual} = props
 
     return (
     <div >
@@ -9,37 +10,37 @@ const Buttons = (props) => {
       <p id="row1">{displayValue}</p>
 
       <div className="row">
-      <button onClick ={handleDisplayOperation} >AC</button>
-      <button onClick ={handleOperation} >%</button>
-      <button onClick ={handleDisplayOperation} >+/-</button>
-      <button onClick ={handleOperation} className="orange">/</button>
+      <button value={displayClear} onClick={handleClear} >{displayClear}</button>
+      <button value="%" onClick={handleOperation} >%</button>
+      <button value="+/-" onClick={handleSign} >+/-</button>
+      <button value="/" onClick ={handleOperation} className="orange">/</button>
       </div>
 
       <div className="row">
-      <button onClick ={handleInput} >7</button>
-      <button onClick ={handleInput}>8</button>
-      <button onClick ={handleInput} >9</button>
-      <button onClick ={handleOperation} className="orange">X</button>
+      <button value="7" onClick={handleInput} >7</button>
+      <button value="8" onClick={handleInput}>8</button>
+      <button value="9" onClick={handleInput} >9</button>
+      <button value="X" onClick={handleOperation} className="orange">X</button>
       </div>
 
       <div className="row">
-      <button onClick ={handleInput} >4</button>
-      <button onClick ={handleInput} >5</button>
-      <button onClick ={handleInput}>6</button>
-      <button onClick ={handleInput} className="orange">-</button>
+      <button value="4" onClick={handleInput} >4</button>
+      <button value="5" onClick={handleInput} >5</button>
+      <button value="6" onClick={handleInput}>6</button>
+      <button value="-" onClick={handleInput} className="orange">-</button>
       </div>
 
       <div className="row">
-      <button onClick ={handleInput} >1</button>
-      <button onClick ={handleInput} >2</button>
-      <button onClick ={handleInput} >3</button>
-      <button onClick ={handleOperation} className=" orange">+</button>
+      <button value="1" onClick={handleInput} >1</button>
+      <button value="2" onClick={handleInput} >2</button>
+      <button value="3" onClick={handleInput} >3</button>
+      <button value="+" onClick={handleOperation} className=" orange">+</button>
       </div>
 
       <div className="row">
-      <button id="zero" onClick ={handleInput} >0</button>
-      <button onClick ={handleDisplayOperation} >.</button>
-      <button onClick ={handleEqual} className=" orange">=</button>
+      <button id="zero" value="0" onClick={handleInput} >0</button>
+      <button value="." onClick={handleDecimalPoint} >.</button>
+      <button value="=" onClick={handleEqual} className=" orange">=</button>
       </div>
 
     </div>
