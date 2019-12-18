71b7d980191c9d945b1a1d16b0cf00843dac1ca9
KameronsCodeSpace <kameronmontague@pursuit.org>
2019-12-17 20:04:37 -0500

    Finished operations and decimal

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -6,29 +6,128 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      displayValue: 0,
+      displayValue: "0",
       previousValue: null,
       operation: null,
-      waitingForNewValue: false
+      waitingForNewValue: false,
+      calculationDone: false
     }
   }
 
-  handleName = (e) => {
+  handleKeyClick = (e) => {
+    const { displayValue, waitingForNewValue, previousValue, calculationDone } = this.state
+    let previousNumber = displayValue
+    let result = displayValue
+    let lastNumbers = previousValue
+    let checkOperator = waitingForNewValue
+
+    console.log('Check Operator', checkOperator)
+    console.log('Check Calculation', calculationDone)
+
+
+    if (calculationDone === true) {
+      result = e.target.value
+      console.log("HIT")
+    } else {
+
+      if (previousNumber === "0" && checkOperator === false) {
+        result = e.target.value
+      } else if (checkOperator === false) {
+        result = previousNumber + e.target.value
+      }
+
+      if (checkOperator) {
+        lastNumbers = displayValue
+        console.log('Lastnumber', lastNumbers)
+        result = e.target.value
+        checkOperator = false
+      }
+    }
     this.setState({
-      todoName: e.target.value
+      displayValue: result,
+      previousValue: lastNumbers,
+      waitingForNewValue: checkOperator,
+      calculationDone: false
+
     })
   }
-  
-  render() {
 
+  handleOperationClick = (e) => {
+    const { operation, waitingForNewValue } = this.state
+
+    let checkOperator = waitingForNewValue
+
+    if (!operation) {
+      checkOperator = true
+    }
+
+    this.setState({
+      operation: e.target.value,
+      waitingForNewValue: checkOperator
+    })
+  }
+
+  handleEqualClick = () => {
+    const { displayValue, previousValue, operation } = this.state
+
+    //FINISHED This thought: decide how to bring the operator here. it's being set to null after a 2nd set of numbers is being clicked
+    let result = displayValue
+
+    if (operation === "+") {
+      result = `${Number(displayValue) + Number(previousValue)}`
+      console.log("Total", result)
+    } else if (operation === "÷") {
+      result = `${Number(previousValue) / Number(displayValue)}`
+
+    } else if (operation === "-") {
+      result = `${Number(previousValue) - Number(displayValue)}`
+
+    } else if (operation === "x") {
+      result = `${Number(previousValue) * Number(displayValue)}`
+    }
+
+    this.setState({
+      displayValue: result,
+      previousValue: null,
+      operation: null,
+      waitingForNewValue: false,
+      calculationDone: true
+    })
+  }
+
+  render() {
+    const { displayValue, previousValue, operation, waitingForNewValue } = this.state
 
     return (
       <div className="App">
 
-        <Calculator />
+        <Calculator
+          displayValue={displayValue}
+          previousValue={previousValue}
+          operation={operation}
+          waitingForNewValue={waitingForNewValue}
+          handleKeyClick={this.handleKeyClick}
+          handleOperationClick={this.handleOperationClick}
+          handleEqualClick={this.handleEqualClick} />
       </div>
     );
   }
 }
 
 export default App;
+
+
+
+
+// this.operations = {
+//   "+": (x, y) => Number(x) + Number(y),
+//   "-": (x, y) => Number(x) - Number(y),
+//   "x": (x, y) => Number(x) * Number(y),
+//   "÷": (x, y) => Number(x) / Number(y),
+// }
+// this.nonOps = {
+//   'AC': (display) => {return {display: 0, operation: null, hold: null}},
+//   '%': (display) => {return {display: Number(display) / 100}},
+//   '±': (display) => {return {display: Number(display) * -1}},
+//   '.': (display) => {return {display: display.toString().includes('.') ? display : display + '.'}},
+// }
--- calculator/src/Components/calculator.jsx
+++ calculator/src/Components/calculator.jsx
@@ -1,36 +1,36 @@
 import React from 'react';
 
 
-const Calculator = () => {
+const Calculator = (props) => {
 
     return (
         <div className="calculator">
-            <input type="text" class="calculator-screen" value="0" disabled />
+            <input type="text" class="calculator-screen" value={props.displayValue} />
 
             <div className="calculator-keys">
             <button value='AC' className="button all-clear">AC</button>
             <button value='%' className="button percent">%</button>
             <button value='±' className="button postive-negative">±</button>
-            <button value='÷' className="button operator-div orange">÷</button>
+            <button value='÷' className="button operator-div orange" onClick={props.handleOperationClick}>÷</button>
 
-            <button value='7' className="button key-7">7</button>
-            <button value='8' className="button key-8">8</button>
-            <button value='9' className="button key-9">9</button>
-            <button value='x' className="button operator-times orange">x</button>
+            <button value='7' className="button key-7" onClick={props.handleKeyClick}>7</button>
+            <button value='8' className="button key-8" onClick={props.handleKeyClick}>8</button>
+            <button value='9' className="button key-9" onClick={props.handleKeyClick}>9</button>
+            <button value='x' className="button operator-times orange" onClick={props.handleOperationClick}>x</button>
 
-            <button value='4' className="button key-4">4</button>
-            <button value='5' className="button key-5">5</button>
-            <button value='6' className="button key-6">6</button>
-            <button value='-' className="button operator-minus orange">-</button>
+            <button value='4' className="button key-4" onClick={props.handleKeyClick}>4</button>
+            <button value='5' className="button key-5" onClick={props.handleKeyClick}>5</button>
+            <button value='6' className="button key-6" onClick={props.handleKeyClick}>6</button>
+            <button value='-' className="button operator-minus orange" onClick={props.handleOperationClick}>-</button>
 
-            <button value='1' className="button key-1">1</button>
-            <button value='2' className="button key-2">2</button>
-            <button value='3' className="button key-3">3</button>
-            <button value='+' className="button operator-plus orange">+</button>
+            <button value='1' className="button key-1" onClick={props.handleKeyClick}>1</button>
+            <button value='2' className="button key-2" onClick={props.handleKeyClick}>2</button>
+            <button value='3' className="button key-3" onClick={props.handleKeyClick}>3</button>
+            <button value='+' className="button operator-plus orange" onClick={props.handleOperationClick}>+</button>
 
-            <button value='0' className="button key-0">0</button>
-            <button value='.' className="button decimal">.</button>
-            <button value='=' className="button equal-sign orange">=</button>
+            <button value='0' className="button key-0" onClick={props.handleKeyClick}>0</button>
+            <button value='.' className="button decimal" onClick={props.handleKeyClick}>.</button>
+            <button value='=' className="button equal-sign orange" onClick={props.handleEqualClick}>=</button>
             </div>
         </div>
     )
